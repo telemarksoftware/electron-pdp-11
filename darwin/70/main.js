2db291f5a35e9ac1c43d2de5f813023987f0c5b2
Mark H. Shin <116744399+telemarksoftware@users.noreply.github.com>
2022-10-27 06:56:11 -0700

    Add files via upload

--- darwin/70/main.js
+++ darwin/70/main.js
@@ -13,8 +13,8 @@ const express = require('express');
 // Our self-contained web server
 const server = express();
 // Listen on port
-server.listen(5070);
-// Serve static files in pdp11 folder:  http://localhost:5070/pdp11.html
+server.listen(1170);
+// Serve static files in pdp11 folder:  http://localhost:1170/pdp11.html
 server.use(express.static(path.join(__dirname, 'pdp11')));
 
 // Register file:// for privileged local file access bypassing Content Security Protocol
@@ -62,7 +62,7 @@ const createWindow = () => {
   });
 
    // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:5070/pdp11.html');
+  mainWindow.loadURL('http://localhost:1170/pdp11.html');
   
 	// Handlers for IPC
 	ipcMain.on('minimize', () => {
